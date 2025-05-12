fee07e0d75d83da48783894229165013c18d9aff
Your Name <you@example.com>
2025-05-12 17:17:45 -0400

    add tuple

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    # Ensure tuples have at least 2 elements, fill missing with 0
+    a1, a2 = (tuple_a + (0,0))[:2]
+    b1, b2 = (tuple_b + (0, 0))[:2]
+
+    # Return the sum of corresponding elements
+    return (a1 + b1, a2 + b2)
+
