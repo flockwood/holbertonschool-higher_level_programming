43dcdf000a7cb3a8b8f8d4f8a569edcb4aae8f6a
Your Name <you@example.com>
2025-05-12 12:37:37 -0400

    print matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integers(matrix=[[]]):
+    for row in matrix:
+        for i, value in enumerate(row):
+            if i != 0:
+                print(" ", end="")
+            print("{:d}".format(value), end="")
+        print()
