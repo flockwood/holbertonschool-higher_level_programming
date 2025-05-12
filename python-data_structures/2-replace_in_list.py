ff7289d198348b840df2933f7ced6d85d5178db1
Your Name <you@example.com>
2025-05-12 11:42:03 -0400

    replace element

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    # Check id idx is negative or out of range
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    
+    # Replace the element at the chosen index
+    my_list[idx] = element
+    return my_list
