abe1cec57e750cc05d7c05e42ecca7eef85480e5
Your Name <you@example.com>
2025-05-19 14:17:40 -0400

    create a square class with private size

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a Square class"""
+
+
+class Square:
+    """Represents a square with a private size attribute."""
+
+    def __init__(self, size):
+        """Initiaize a new Sqaure with a given size."""
+        self.__size = size
