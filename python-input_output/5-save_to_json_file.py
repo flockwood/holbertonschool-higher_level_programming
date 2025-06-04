db701d754e044e247a3535872e7ec58755690644
flockwood <flockwood@live.com>
2025-06-04 14:17:15 -0400

    fix typo

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -17,5 +17,5 @@ def save_to_json_file(my_obj, filename):
         my_obj: The object to serialize and save to file.
         filename (str): The name of the file to write to.
     """
-    with open(filename, 'w', enconding='utf-8') as file:
+    with open(filename, 'w', encoding='utf-8') as file:
         json.dump(my_obj, file)
