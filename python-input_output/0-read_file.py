266fe1dab066f00ce672f88875d4f7a520b022dd
flockwood <flockwood@live.com>
2025-06-04 11:40:32 -0400

    fixed pycodestyle

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 
 """
-This modile defines a single function, read_file
-which reads a UTF-8 encoded text file and prints 
-its contents to stdout, exactly as it appears on
+This module defines a single function, read_file.
+
+The function reads a UTF-8 encoded text file and prints
+its contents to stdout, exactly as it appears in
 the file. No error handling is needed.
 """
 
 
 def read_file(filename=''):
     """
-    Reads a UTF-8 text file and prints it to stdout.
+    Read a UTF-8 text file and print it to stdout.
 
     Args:
-        filename (str): The name of the file to read
+        filename (str): The name of the file to read.
     """
     with open(filename, 'r', encoding='utf-8') as file:
         content = file.read()
