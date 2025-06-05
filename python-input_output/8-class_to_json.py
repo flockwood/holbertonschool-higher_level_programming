8130da12c42c297b75473d9051411ab019fa8735
flockwood <flockwood@live.com>
2025-06-05 12:06:30 -0400

    function returns the dic

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module defines a single function, class_to_json.
+
+The function returns the dictionary description with simple data structure
+for JSOn serialization of an object. All attributes are assumed to be
+serializable.
+"""
+
+def class_to_json(obj):
+    """
+    Return the dictionary description of an object for JSOn serialization.
+
+    Args:
+        obj: An instance of a Class with serializable attributes.
+
+    Returns:
+        dict: Dictionary containing all serializable attributes of the object.
+    """
+    return obj.__dict__
