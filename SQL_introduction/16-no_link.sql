a4f7d2e2c8eaf9f18f06383af6498972de1e3b56
flockwood <flockwood@live.com>
2025-06-25 13:43:15 -0400

    no links

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- Script that lists all records of the table second_table
+-- Don't list rows where the name column does not contain a value
+-- Results should display the score and the name (in this order)
+-- Records should be listed by descending score
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
