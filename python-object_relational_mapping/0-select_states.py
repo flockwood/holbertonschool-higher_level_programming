e29a00c02c0cf667642baa34f6005d02a4bf431e
flockwood <flockwood@live.com>
2025-06-27 15:48:10 -0400

    fix

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,39 +1,27 @@
 #!/usr/bin/python3
 """
-List all states from the databasfe hbtn_0e_usa
-Usage: ./0-select_states.py <username> <password> <database>
+Script that lists all states from the database hbtn_0e_0_usa
 """
-
 
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    # Get credentials from command line arguments
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    db_name = sys.argv[3]
-
-    # Connect MySQL server on localhost at port 3306
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=user,
-        passwd=passwd,
-        db=db_name
-    )
-
-    # Create a cursor to execute SQL queries
-    cursor = db.cursor()
-
-    # Execute SQL query
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Fetch and print all rows
-    for row in cursor.fetchall():
-        print(row)
-
-    # Clean up
-    cursor.close()
-    db.close()
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            charset="utf8"
+        )
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+        cursor.close()
+        db.close()
+    except MySQLdb.Error:
+        pass
