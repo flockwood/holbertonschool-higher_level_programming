745ccfd7b80b962668ee680212e78d93fc152b03
flockwood <flockwood@live.com>
2025-07-17 12:05:17 -0400

    added .red class

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,4 @@
+// Add click event listener to the red_header element
+document.querySelector('#red_header').addEventListener('click', function() {
+    document.querySelector('header').classList.add('red');
+});
