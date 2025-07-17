dfe7add3059d142ac192e088791b70623d072d09
flockwood <flockwood@live.com>
2025-07-17 12:15:10 -0400

    change the text

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,4 @@
+// Add click event listener to the update_header element
+document.querySelector('#update_header').addEventListener('click', function() {
+    document.querySelector('header').textContent = 'New Header!!!';
+});
