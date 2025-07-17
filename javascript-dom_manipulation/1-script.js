cebc4ff23fe35fdd0c11969212f2bec5ad5aa1e8
flockwood <flockwood@live.com>
2025-07-17 11:59:59 -0400

    added main html files for 0,1. click and turn script as well

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,4 @@
+// Add click event listener to the red_header element
+document.querySelector('#red_header').addEventListener('click', function() {
+    document.querySelector('header').style.color = '#FF0000';
+});
